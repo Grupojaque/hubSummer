831d8ad46eb072a4aec96501f6df42808baacda8
krvax <krva@ciencias.unam.mx>
2014-07-16 13:08:58 -0500

    Cambios al servidor de Mailgun.

--- MaterialCurso/cloudComputing/servidorMailgun.js
+++ MaterialCurso/cloudComputing/servidorMailgun.js
@@ -1,19 +1,9 @@
 var express=require('express');
 app=express();
 
-
-
 var api_key = 'key-5wds68amszmbnm6h98xmx3m0q5f0i576';
 var domain = 'sandbox6f1d638aab7c41bcaec841ef13e17bdd.mailgun.org';
 var mailgun = require('mailgun-js')(api_key,domain);
-
-
-var data = {
-  from: 'Miguel <me@samples.mailgun.org>',
-  to: 'miguel.carvajal@grupojaque.com',
-  subject: 'Prueba Mailgun',
-  text: 'Testing some Mailgun awesomness!'
-};
 
 app.post('/mailgun', 
         function(request, response) 
@@ -27,12 +17,9 @@ app.post('/mailgun',
                                         'Content-Type': 'text/plain',
                                         'Access-Control-Allow-Origin': '*' 
                                 });
-                                response.end('sent');
+                                response.end('Mensaje enviado.');
                         });
         });
 
-
-
-
 app.listen(9000);
-
+console.log('Escuchando en el puerto 9000');
